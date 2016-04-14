94630c036f93679bcad979e1e2b19b7d2de98746
karael <owickarael@gmail.com>
2016-04-14 16:40:01 +0200

    pushing doc site

---
+++ siteDoc/src/components/app-header.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="app-header">
+        <div class="row">
+            <nav class="navbar">
+                <div class="container-fluid">
+                    <div class="hidden-xs hidden-sm navbar-header">
+                        <a class="navbar-brand" v-link="{ path : '/home' }">Anthologie palatine</a>
+                    </div>
+                    <div class="nav navbar-nav navbar-right">
+                        <ul class="list-inline col-xs-9 col-md-10 text-right">
+                            <li><a v-link="{ path : '/home' }" v-link-active>Accueil</a></li>
+                            <li><a v-link="{ path : '/documentation' }">Documentation</a></li>
+                            <li class="hidden-xs hidden-sm"><a href="#">Accès API</a></li>
+                            <li class="hidden-xs hidden-sm"><a href="#">Exemple</a></li>
+                        </ul>
+                        <div class="col-xs-3 col-md-2">
+                            <select name="lang" id="lang" class="form-control">
+                                <option value="fr">Fr</option>
+                                <option value="en">En</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<style lang="sass">
+$default-color: #366870;
+$bg-color: #e6e6e6;
+
+body{
+    background: $bg-color;
+}
+    
+.app-header{
+    height: 60px;
+    line-height: 60px;
+    border-bottom: 1px solid $default-color;
+    margin: 0 -15px;
+    background: #fff;
+      
+    .navbar-header{
+        padding-left: 50px;
+          
+        .navbar-brand{
+            color: $default-color;
+            font-weight: 700;
+            transition: .2s all linear;
+            font-size: 28px;
+              
+            &:hover,
+            &:focus{
+                text-decoration: none;
+                opacity: .8;
+            }
+        }
+    }
+      
+    ul{
+        padding: 0;
+          
+        li{
+              
+            a{
+                font-size: 16px;                                      
+                color: #000;
+                border-bottom: 2px solid transparent;
+                transition: .2s all linear;
+                margin: 0;   
+                               
+                &:hover,
+                &:focus,
+                &.v-link-active{
+                    text-decoration: none;
+                    border-bottom: 2px solid $default-color;
+                    color: #000; 
+                }
+            }
+                
+            @media (min-width: 992px) { 
+                
+                a{
+                    font-size: 24px;
+                    margin: 0 12px;
+                }                
+            }
+        }
+    }
+      
+    select.form-control{
+        border: none;
+        opacity: 0.5;
+        display: inline-block;
+        font-size: 18px;
+        width: 72%;
+        cursor: pointer;
+        padding: 0;
+    }
+    
+    @media (min-width: 768px) {
+        
+        .navbar-right{
+            width: 100%;
+        }   
+        
+        select.form-control{
+            width: 50%;                        
+        }           
+    }
+    
+    @media (min-width: 992px) {
+        
+        .navbar-right{
+            width: 60%;
+        }
+        
+        select.form-control{
+            width: 50%;   
+        }
+    }
+} 
+
+</style>
---
+++ siteDoc/src/components/doc-content.vue
@@ -0,0 +1,274 @@
+<template>
+    <div class="doc-content col-sm-8 col-md-9">
+        <div class="content-container">
+            <div class="title-container">
+                <h3>{{ dataContent.title }}</h3>
+            </div>
+            <div class="subtitle-wrapper" v-for="section in dataContent.section">
+                <div class="subtitle-container">
+                    <h4 id="{{ section.slug }}">{{ section.subtitle }}</h4>
+                </div>
+                <div class="text-container">
+                    <code><span class="text-uppercase">{{ section.method }} </span><em>{{ section.url.mandatory }}</em> <span class="opt"><em v-for="opt in section.url.opt">{{ opt }} </em></span></code>
+                    <p>{{ section.desc }}</p>
+                    <div class="row">
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <caption>Paramètres</cpation>
+                                <thead>
+                                    <tr>
+                                        <th>Champs</th>
+                                        <th>Type</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody v-for="param in section.param">
+                                    <tr v-for="mandatory in param.mandatory">
+                                        <td>{{ mandatory.field }}</td>
+                                        <td>{{ mandatory.type }}</td>
+                                        <td>{{ mandatory.desc }}</td>
+                                    </tr>
+                                    <tr v-for="opt in param.opt">
+                                        <td>{{ opt.field }}</td>
+                                        <td>{{ opt.type }}</td>
+                                        <td>{{{ opt.desc }}}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <caption>Success</cpation>
+                                <thead>
+                                    <tr>
+                                        <th>Champs</th>
+                                        <th>Type</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="success in section.reponses.success">
+                                        <td>{{ success.field }}</td>
+                                        <td>{{ success.type }}</td>
+                                        <td>{{ success.desc }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <caption>Errors</cpation>
+                                <thead>
+                                    <tr>
+                                        <th>Number</th>
+                                        <th>Title</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="error in section.reponses.errors">
+                                        <td>{{ error.number }}</td>
+                                        <td>{{ error.title }}</td>
+                                        <td>{{ error.desc }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+    
+  props: {
+    dataContent: Object
+  },
+  
+  data () {
+    return {
+      data: {}
+    }
+  }
+
+}
+</script>
+
+<style lang="sass">
+$default-color: #366870;
+$bg-code: #434343;
+$grey: #848484;
+
+.doc{
+    position: relative;
+    margin: 0 -15px;
+    
+    >.row{
+        padding-top: 5%;
+    }
+}
+
+nav.doc-nav{
+    margin: 0;
+    
+    .navbar-header{
+        
+        button{
+            
+            .glyphicon{
+                font-size: 30px;              
+            }
+            
+            .glyphicon-menu-down{
+                display: initial;
+            }
+            
+            .glyphicon-menu-up{
+                display: none;
+            }
+            
+            &[aria-expanded="true"]{               
+                
+                .glyphicon-menu-down{
+                    display: none;
+                }
+                
+                .glyphicon-menu-up{
+                    display: initial;
+                }
+            }
+        }
+    }
+    
+    .navbar-collapse{
+        border-bottom: 1px solid $default-color;
+        padding: 0 10% 10%;
+        margin-bottom: 10%;
+        
+        ul{
+            
+            li{
+                margin: 5% 0;
+                padding: 0;
+                
+                a{
+                    font-size: 24px;
+                    font-weight: 700;
+                    color: $grey;
+                    transition: .2s all linear;
+                    
+                    &:hover,
+                    &.v-link-active{
+                        color: $default-color;
+                        text-decoration: none;
+                    }
+                    
+                    &.v-link-active{
+                        
+                        ~ul{
+                            display: block;
+                        }
+                    }
+                }
+                
+                ul{
+                    list-style: none;
+                    display: none;
+                    
+                    li{
+                        
+                        a{
+                            font-size: 15px;
+                            font-weight: 400;
+                            color: $default-color;
+                            opacity: .4;
+                            
+                            &:hover{
+                                opacity: 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @media (min-width: 768px){
+        
+        .navbar-collapse{
+            border: none;
+        }
+    }
+}
+
+.content-container{
+    padding: 0 10%;
+    margin-bottom: 50px;
+    
+    .subtitle-container,
+    .text-container{
+        margin: 5% 0;
+    }
+    
+    .title-container{
+        margin: 0 0 10%;
+        
+        h3{
+            font-size: 36px;
+            font-weight: 700;
+            margin-top: 0;
+        }
+    }
+    
+    .subtitle-container{
+        
+        h4{
+            font-size: 24px;            
+        }        
+    }
+    
+    table{
+        
+        td{
+            opacity: .6;
+        }
+    }
+    
+    .text-container{
+        
+        p{
+            font-size: 16px;
+            color: #000;
+            line-height: 1.6;
+            margin: 5% 0 10%;
+        }
+        
+        code{
+            background: $bg-code;
+            border-radius: 0;
+            color: #fff;
+            padding: 2% 5%;
+            font-family: 'Inconsolata', sans-serif;
+            width: 100%;
+            display: inline-block;
+            overflow-x: scroll;
+            white-space: nowrap;
+            font-size: 18px;
+            
+            .opt{
+                color: #4c949f;
+            }
+        }
+    }
+}
+
+</style>
