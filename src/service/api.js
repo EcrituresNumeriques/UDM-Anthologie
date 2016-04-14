728fba41653bd28e6107f36ae84886b62a3dfbc3
karael <owickarael@gmail.com>
2016-04-14 16:41:41 +0200

    add readme and gitignore

--- src/service/api.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-const host = 'localhost:8080/src/'
-
-exports.dataHome = Vue.resource(host+'mock/home.json');
-exports.dataNav = Vue.resource(host+'mock/doc-nav.json');
-exports.baseUrl = host+'mock/doc/';
