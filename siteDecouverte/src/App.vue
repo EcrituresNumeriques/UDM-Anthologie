9a7334fef6edf0d27dffb40bb745d7b7e21e48f8
Valentin Crochemore <valentin.crochemore@gmail.com>
2016-05-05 18:25:10 +0200

    Mise en place de la page d'accueil

--- siteDecouverte/src/App.vue
+++ siteDecouverte/src/App.vue
@@ -1,64 +1,33 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+  <div id="app" class="container-fluid">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Home from './components/Home'
 
 export default {
   components: {
-    Hello
+    Home
   }
 }
 </script>
 
-<style>
-html {
-  height: 100%;
-}
+<style lang="sass">
+@font-face
+  font-family: 'times'
+  src: url(~assets/fonts/times/Times-Roman.ttf)
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
+html
+  height: 100%
 
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
+body
+  display: flex
+  height: 100%
+  font-family: 'times', Helvetica, Arial, sans-serif
+  
+#app
+  width: 100%
+  height: 100%    
 </style>
--- siteDecouverte/src/main.js
+++ siteDecouverte/src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
+// vue-router for routing ...
+import Router from 'vue-router'
+// import app component
 import App from './App'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+// import components
+import Home from './components/Home.vue'
+
+// install router
+Vue.use(Router)
+
+// routing
+export var router = new Router({
+  history: true
 })
+
+router.map({
+  '/': {
+    component: Home,
+    name: 'home'
+  },
+  '/accueil': {
+    component: Home,
+    name: 'home'
+  }
+})
+
+// start the app
+router.start(App, 'app')
