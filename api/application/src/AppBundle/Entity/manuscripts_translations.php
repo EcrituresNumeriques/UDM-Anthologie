a65c9d50ff303e56827d8a6719b0e5ca5f2a5049
karael <owickarael@gmail.com>
2016-05-23 13:55:07 +0200

    updating entities

---
+++ api/application/src/AppBundle/Entity/manuscripts_translations.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * manuscripts_translations
+ *
+ * @ORM\Table(name="manuscripts_translations")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\manuscripts_translationsRepository")
+ */
+class manuscripts_translations
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="page", type="integer")
+     */
+    private $page;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return manuscripts_translations
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set page
+     *
+     * @param integer $page
+     *
+     * @return manuscripts_translations
+     */
+    public function setPage($page)
+    {
+        $this->page = $page;
+
+        return $this;
+    }
+
+    /**
+     * Get page
+     *
+     * @return int
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+}
+
