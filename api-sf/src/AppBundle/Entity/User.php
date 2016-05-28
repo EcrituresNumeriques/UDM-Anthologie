7ed63034a38d019aad420befccd1c9a32463a684
karael <owickarael@gmail.com>
2016-05-28 18:53:17 +0200

    moving dockerfiles & removing sf3

---
+++ api-sf/src/AppBundle/Entity/User.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $apiKey;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $university;
+
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword()
+    {
+    }
+    public function getSalt()
+    {
+    }
+    public function eraseCredentials()
+    {
+    }
+
+    // more getters/setters
+}
