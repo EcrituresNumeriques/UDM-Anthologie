769851134440ca5f545f45201922e19e86f0c56c
karael <owickarael@gmail.com>
2016-06-07 16:49:53 +0200

    updating manual command for OAuth token creation

--- api-sf2/src/Acme/ApiBundle/Command/ClientCreateCommand.php
+++ api-sf2/src/Acme/ApiBundle/Command/ClientCreateCommand.php
@@ -4,8 +4,8 @@ namespace Acme\ApiBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ClientCreateCommand extends ContainerAwareCommand
@@ -13,21 +13,42 @@ class ClientCreateCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this
-            ->setName('api:oauth-server:client-create')
-            ->setDescription('Create a new client')
-            ->addArgument('name', InputArgument::REQUIRED, 'Sets the client name', null)
-            ->addOption('redirect-uri', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Sets redirect uri for client. Use this option multiple times to set multiple redirect URIs.', null)
-            ->addOption('grant-type', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Sets allowed grant type for client. Use this option multiple times to set multiple grant types.', null)
-        ;
+            ->setName('acme:oauth-server:client:create')
+            ->setDescription('Creates a new client')
+            ->addOption(
+                'redirect-uri',
+                null,
+                InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
+                'Sets redirect uri for client. Use this option multiple times to set multiple redirect URIs.',
+                null
+            )
+            ->addOption(
+                'grant-type',
+                null,
+                InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
+                'Sets allowed grant type for client. Use this option multiple times to set multiple grant types..',
+                null
+            )
+            ->setHelp(
+                <<<EOT
+                    The <info>%command.name%</info>command creates a new client.
+<info>php %command.full_name% [--redirect-uri=...] [--grant-type=...] name</info>
+EOT
+            );
     }
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $clientManager = $this->getContainer()->get('fos_oauth_server.client_manager.default');
         $client = $clientManager->createClient();
-        $client->setName($input->getArgument('name'));
         $client->setRedirectUris($input->getOption('redirect-uri'));
         $client->setAllowedGrantTypes($input->getOption('grant-type'));
         $clientManager->updateClient($client);
-        $output->writeln(sprintf('Added a new client with name <info>%s</info> and public id <info>%s</info>.', $client->getName(), $client->getPublicId()));
+        $output->writeln(
+            sprintf(
+                'Added a new client with public id <info>%s</info>, secret <info>%s</info>',
+                $client->getPublicId(),
+                $client->getSecret()
+            )
+        );
     }
 }
