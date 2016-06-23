5d303653a2f8b21bf942027642d5fc83909b06b2
karael <owickarael@gmail.com>
2016-06-23 17:11:27 +0200

    updated relationships

--- api-sf2/src/AppBundle/Entity/Eras.php
+++ api-sf2/src/AppBundle/Entity/Eras.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\OneToMany;
 
 /**
  * Eras
@@ -36,5 +38,105 @@ class Eras
     private $dateEnd;
 
 
-}
+    /**
+     * @OneToMany(targetEntity="ErasTranslation", mappedBy="era")
+     */
+    private $eraTranslations;
 
+
+    public function __construct() {
+        $this->features = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set dateBegin
+     *
+     * @param integer $dateBegin
+     *
+     * @return Eras
+     */
+    public function setDateBegin($dateBegin)
+    {
+        $this->dateBegin = $dateBegin;
+
+        return $this;
+    }
+
+    /**
+     * Get dateBegin
+     *
+     * @return integer
+     */
+    public function getDateBegin()
+    {
+        return $this->dateBegin;
+    }
+
+    /**
+     * Set dateEnd
+     *
+     * @param integer $dateEnd
+     *
+     * @return Eras
+     */
+    public function setDateEnd($dateEnd)
+    {
+        $this->dateEnd = $dateEnd;
+
+        return $this;
+    }
+
+    /**
+     * Get dateEnd
+     *
+     * @return integer
+     */
+    public function getDateEnd()
+    {
+        return $this->dateEnd;
+    }
+
+    /**
+     * Add eraTranslation
+     *
+     * @param \AppBundle\Entity\ErasTranslation $eraTranslation
+     *
+     * @return Eras
+     */
+    public function addEraTranslation(\AppBundle\Entity\ErasTranslation $eraTranslation)
+    {
+        $this->eraTranslations[] = $eraTranslation;
+
+        return $this;
+    }
+
+    /**
+     * Remove eraTranslation
+     *
+     * @param \AppBundle\Entity\ErasTranslation $eraTranslation
+     */
+    public function removeEraTranslation(\AppBundle\Entity\ErasTranslation $eraTranslation)
+    {
+        $this->eraTranslations->removeElement($eraTranslation);
+    }
+
+    /**
+     * Get eraTranslations
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getEraTranslations()
+    {
+        return $this->eraTranslations;
+    }
+}
--- api-sf2/src/AppBundle/Entity/ErasTranslation.php
+++ api-sf2/src/AppBundle/Entity/ErasTranslation.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
 
 /**
  * ErasTranslation
@@ -15,11 +17,11 @@ class ErasTranslation
     /**
      * @var integer
      *
-     * @ORM\Column(name="id_era_name", type="integer")
+     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="NONE")
      */
-    private $idEraName;
+    private $id;
 
     /**
      * @var string
@@ -41,6 +43,160 @@ class ErasTranslation
      * @ORM\Column(name="description", type="text", length=65535, nullable=true)
      */
     private $description;
-    
-}
 
+    /**
+     * @ManyToOne(targetEntity="Eras", inversedBy="eraTranslations")
+     * @JoinColumn(name="era_id", referencedColumnName="id")
+    */
+    private $era;
+
+    /**
+     * @ManyToOne(targetEntity="Languages")
+     * @JoinColumn(name="language_id", referencedColumnName="id")
+     */
+    private $language;
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     *
+     * @return ErasTranslation
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return ErasTranslation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set cultureCenters
+     *
+     * @param string $cultureCenters
+     *
+     * @return ErasTranslation
+     */
+    public function setCultureCenters($cultureCenters)
+    {
+        $this->cultureCenters = $cultureCenters;
+
+        return $this;
+    }
+
+    /**
+     * Get cultureCenters
+     *
+     * @return string
+     */
+    public function getCultureCenters()
+    {
+        return $this->cultureCenters;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return ErasTranslation
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set era
+     *
+     * @param \AppBundle\Entity\Eras $era
+     *
+     * @return ErasTranslation
+     */
+    public function setEra(Eras $era = null)
+    {
+        $this->era = $era;
+
+        return $this;
+    }
+
+    /**
+     * Get era
+     *
+     * @return \AppBundle\Entity\Eras
+     */
+    public function getEra()
+    {
+        return $this->era;
+    }
+
+    /**
+     * Set language
+     *
+     * @param \AppBundle\Entity\Languages $language
+     *
+     * @return ErasTranslation
+     */
+    public function setLanguage(Languages $language = null)
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    /**
+     * Get language
+     *
+     * @return \AppBundle\Entity\Languages
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+}
