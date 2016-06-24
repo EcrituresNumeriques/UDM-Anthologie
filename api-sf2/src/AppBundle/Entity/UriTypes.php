94fdc36cf779dbc01301a1f2205b12651a5ad7e5
karael <owickarael@gmail.com>
2016-06-24 18:22:56 +0200

    fix getters and setters

--- api-sf2/src/AppBundle/Entity/UriTypes.php
+++ api-sf2/src/AppBundle/Entity/UriTypes.php
@@ -2,16 +2,29 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Model as ORMBehaviors;
+
 /**
  * UriTypes
+ *
+ * @ORM\Table(name="URI_types")
+ * @ORM\Entity
  */
 class UriTypes
 {
+    use ORMBehaviors\SoftDeletable\SoftDeletable,
+        ORMBehaviors\Timestampable\Timestampable
+    ;
     /**
      * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
-
+    
 
     /**
      * Get id
@@ -23,4 +36,3 @@ class UriTypes
         return $this->id;
     }
 }
-
