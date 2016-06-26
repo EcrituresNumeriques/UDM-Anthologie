6670f4912754cfe5e0f43f7676705b854264def7
karael <owickarael@gmail.com>
2016-06-26 12:49:58 +0200

    fixed configuration and types of datas

--- api-sf2/src/AppBundle/Form/AuthorsTranslationsType.php
+++ api-sf2/src/AppBundle/Form/AuthorsTranslationsType.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,10 +17,14 @@ class AuthorsTranslationsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('about')
+            ->add('name', TextType::class, array(
+                'required' => true
+            ))
+            ->add('about',TextType::class, array(
+                'required' => false
+            ))
             ->add('language')
-        ;
+            ->add('group');
     }
     
     /**
@@ -28,7 +34,8 @@ class AuthorsTranslationsType extends AbstractType
     {
         $resolver->setDefaults(array(
             'data_class' => 'AppBundle\Entity\AuthorsTranslations',
-            'csrf_protection' => false
+            'csrf_protection' => false,
+            'allow_extra_fields' => true
         ));
     }
 
